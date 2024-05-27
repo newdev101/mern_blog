99a6d129696799b3a972b34480d10ae2c44c71a2
rajat jana <rajatjana2024@gmail.com>
2024-05-27 17:26:45 +0530

    db connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+.connect(process.env.mongo_uri)
+.then(()=>console.log('db connected'))
+.catch((err)=>console.log(err))
 
 const PORT = 3000;
 const app = express();
