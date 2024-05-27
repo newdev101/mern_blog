600b225b457434f0e5285528b6613af021d4676e
rajat jana <rajatjana2024@gmail.com>
2024-05-27 18:03:04 +0530

    created and tested test route

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
+
+//todo%%%%%%%%%% Database connection %%%%%%%%%%%%%
 mongoose
 .connect(process.env.mongo_uri)
 .then(()=>console.log('db connected'))
 .catch((err)=>console.log(err))
 
+
+//todo%%%%%%%%%% server connection %%%%%%%%%%%%%
 const PORT = 3000;
 const app = express();
+
+app.use('/api/user',userRoutes)
+
 app.listen(PORT, () => console.log(`server started on PORT:${PORT}`));
 
