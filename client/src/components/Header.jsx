1e744890a39ec75408f2cd17b5c2763faff7686b
rajat jana <rajatjana2024@gmail.com>
2024-05-27 16:38:58 +0530

    created server in root

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,7 +34,7 @@ function Header() {
                          <FaMoon />
                     </Button>
                     <Link>
-                         <Button gradientDuoTone={'purpleToBlue'} pill>
+                         <Button gradientDuoTone={'purpleToBlue'} outline pill>
                               Sign In
                          </Button>
                     </Link>
